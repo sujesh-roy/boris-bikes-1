9b316dd800597e6e514c759c1f62a52a26a2c0b4
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-10-31 16:07:22 +0000

    update bike and docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,8 @@ require './lib/docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
   end
